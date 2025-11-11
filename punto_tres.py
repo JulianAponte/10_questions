7a03e81467f5ed2789dd42c66c443cd016e0397a
Julián Aponte <aponteapps@gmail.com>
2025-11-11 07:00:12 -0500

    Fix input validation and improve training feedback

--- punto_tres.py
+++ punto_tres.py
@@ -2,24 +2,32 @@
 
 flag_1 = True
 
-days_training = input("Enter quantity that you days training last week: ")
-
 while flag_1:
 
-    if not 0 <= days_training.isdigit() <= 7:
-        print("Invalid number, please enter a valid number days.")
+    days_training = input("Enter quantity that you days training last week: ")
+
+    if not days_training.isdigit():
+        print("\nInvalid number, please enter a valid number days.\n")
         continue
 
+    if 0 <= days_training.isnumeric() <= 7:
 
-    days_training = int(days_training)
+        days_training = int(days_training)
 
-    if 0 <= days_training <= 1:
-        print("Don't give up, ¡You can do better!")
+        if 0 <= days_training <= 1:
+            print("Don't give up, ¡You can do better!")
+            break
 
-    elif 2 <= days_training <= 3:
-        print("Good job, ¡But you can give more!")
-    
+        elif 2 <= days_training <= 3:
+            print("Good job, ¡But you can give more!")
+            break
+
+        elif 4 <= days_training <= 7:
+            print("¡Excelent! You win a skill point (Skill point +1)")
+            break
+
+        else:
+            print('\nInvalid quantity, please enter a valid quantity.\n')
+            continue
     else:
-        print("¡Excelent! You win a skill point (Skill point +1)")
-    
-    break
+        break
